0237e57fffd6505237ee38679691d2c7ea0fd3d7
BenjaminHun <papp.benjamin.hun@gmail.com>
2024-02-08 08:41:32 +0100

    transfer learning, alexnet, last layer doubled

--- FlowNetPytorch/models/FlowNetC.py
+++ FlowNetPytorch/models/FlowNetC.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn.init import kaiming_normal_, constant_
 from .util import conv, predict_flow, deconv, crop_like, correlate
+import torchvision
 
 __all__ = [
     'flownetc', 'flownetc_bn'
@@ -14,10 +15,17 @@ class FlowNetC(nn.Module):
     def __init__(self, batchNorm=True):
         super(FlowNetC, self).__init__()
 
+        self.alexNetEncoder = torchvision.models.alexnet(
+            pretrained=True).features
+        self.alexNetEncoder.classifier = nn.Identity()
+        self.alexNetEncoder.avgpool = nn.Identity()
+        for param in self.alexNetEncoder.parameters():
+            param.requires_grad = False
+
         self.batchNorm = batchNorm
-        self.conv1 = conv(self.batchNorm,   3,   64, kernel_size=7, stride=2)
-        self.conv2 = conv(self.batchNorm,  64,  128, kernel_size=5, stride=2)
-        self.conv3 = conv(self.batchNorm, 128,  256, kernel_size=5, stride=2)
+        # self.conv1 = conv(self.batchNorm,   3,   64, kernel_size=7, stride=2)
+        # self.conv2 = conv(self.batchNorm,  64,  128, kernel_size=5, stride=2)
+        # self.conv3 = conv(self.batchNorm, 128,  256, kernel_size=5, stride=2)
         self.conv_redir = conv(self.batchNorm, 256,   32,
                                kernel_size=1, stride=1)
 
@@ -38,7 +46,7 @@ class FlowNetC(nn.Module):
         self.predict_flow5 = predict_flow(1026)
         self.predict_flow4 = predict_flow(770)
         self.predict_flow3 = predict_flow(386)
-        self.predict_flow2 = predict_flow(194)
+        self.predict_flow2 = predict_flow(322)
 
         self.upsampled_flow6_to_5 = nn.ConvTranspose2d(
             2, 2, 4, 2, 1, bias=False)
@@ -58,18 +66,25 @@ class FlowNetC(nn.Module):
                 constant_(m.weight, 1)
                 constant_(m.bias, 0)
 
+    def print_sizes(self, model, input_tensor):
+        output = input_tensor
+        for m in model.children():
+            output = m(output)
+        return output
+
     def forward(self, x):
         x1 = x[:, :3]
         x2 = x[:, 3:]
 
-        out_conv1a = self.conv1(x1)
-        out_conv2a = self.conv2(out_conv1a)
-        out_conv3a = self.conv3(out_conv2a)
+        # out_conv1a = self.conv1(x1)
+        # out_conv2a = self.conv2(out_conv1a)
+        # out_conv3a = self.conv3(out_conv2a)
+        out_conv3a = self.alexNetEncoder(x1)
 
-        out_conv1b = self.conv1(x2)
-        out_conv2b = self.conv2(out_conv1b)
-        out_conv3b = self.conv3(out_conv2b)
-
+        # out_conv1b = self.conv1(x2)
+        # out_conv2b = self.conv2(out_conv1b)
+        # out_conv3b = self.conv3(out_conv2b)
+        out_conv3b = self.alexNetEncoder(x2)
         out_conv_redir = self.conv_redir(out_conv3a)
         out_correlation = correlate(out_conv3a, out_conv3b)
 
@@ -96,10 +111,10 @@ class FlowNetC(nn.Module):
 
         concat3 = torch.cat((out_conv3, out_deconv3, flow4_up), 1)
         flow3 = self.predict_flow3(concat3)
-        flow3_up = crop_like(self.upsampled_flow3_to_2(flow3), out_conv2a)
-        out_deconv2 = crop_like(self.deconv2(concat3), out_conv2a)
+        flow3_up = crop_like(self.upsampled_flow3_to_2(flow3), out_conv3a)
+        out_deconv2 = crop_like(self.deconv2(concat3), out_conv3a)
 
-        concat2 = torch.cat((out_conv2a, out_deconv2, flow3_up), 1)
+        concat2 = torch.cat((out_conv3a, out_deconv2, flow3_up), 1)
         flow2 = self.predict_flow2(concat2)
 
         if self.training:
