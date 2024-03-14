7b4c18e01a203a06477b1818984b7f6ef91d0094
BenjaminHun <papp.benjamin.hun@gmail.com>
2024-03-14 08:07:34 +0100

    convnext

--- FlowNetPytorch/models/FlowNetC.py
+++ FlowNetPytorch/models/FlowNetC.py
@@ -1,10 +1,8 @@
 import torch
 import torch.nn as nn
 from torch.nn.init import kaiming_normal_, constant_
-import math as m
-from swin_transformer_custom import SwinTransformer
 from .util import conv, predict_flow, deconv, crop_like, correlate
-import torchvision
+from .ConvNext import ConvNeXt
 __all__ = [
     'flownetc', 'flownetc_bn'
 ]
@@ -15,32 +13,26 @@ class FlowNetC(nn.Module):
 
     def __init__(self, batchNorm=True):
         super(FlowNetC, self).__init__()
-        model = torchvision.models.swin_t(
-            weights=torchvision.models.Swin_T_Weights).features
-        layers = []
+        self.model = ConvNeXt(in_chans=3, depths=[
+                              1, 1, 3], dims=[64, 128, 256])
 
-        for m in model.children():
-            layers.append(m)
+        self.model2 = ConvNeXt(in_chans=256, depths=[1, 1, 1], dims=[
+            512, 512, 1024])
 
-        self.backboneModel = nn.Sequential(*layers[:2])
-
-        for param in self.backboneModel.parameters():
-            param.requires_grad = False
-        
         self.batchNorm = batchNorm
         # self.conv1 = conv(self.batchNorm,   3,   64, kernel_size=7, stride=2)
         # self.conv2 = conv(self.batchNorm,  64,  128, kernel_size=5, stride=2)
-        self.conv3 = conv(self.batchNorm, 96,  256, kernel_size=5, stride=2,resnet=False)
+        # self.conv3 = conv(self.batchNorm, 128,  256, kernel_size=5, stride=2)
         self.conv_redir = conv(self.batchNorm, 256,   32,
-                               kernel_size=1, stride=1,resnet=False)
+                               kernel_size=1, stride=1)
 
         self.conv3_1 = conv(self.batchNorm, 473,  256)
         self.conv4 = conv(self.batchNorm, 256,  512, stride=2)
-        #self.conv4_1 = conv(self.batchNorm, 512,  512)
+        self.conv4_1 = conv(self.batchNorm, 512,  512)
         self.conv5 = conv(self.batchNorm, 512,  512, stride=2)
-        #self.conv5_1 = conv(self.batchNorm, 512,  512)
+        self.conv5_1 = conv(self.batchNorm, 512,  512)
         self.conv6 = conv(self.batchNorm, 512, 1024, stride=2)
-        #self.conv6_1 = conv(self.batchNorm, 1024, 1024)
+        self.conv6_1 = conv(self.batchNorm, 1024, 1024)
 
         self.deconv5 = deconv(1024, 512)
         self.deconv4 = deconv(1026, 256)
@@ -51,7 +43,7 @@ class FlowNetC(nn.Module):
         self.predict_flow5 = predict_flow(1026)
         self.predict_flow4 = predict_flow(770)
         self.predict_flow3 = predict_flow(386)
-        self.predict_flow2 = predict_flow(162)
+        self.predict_flow2 = predict_flow(194)
 
         self.upsampled_flow6_to_5 = nn.ConvTranspose2d(
             2, 2, 4, 2, 1, bias=False)
@@ -74,45 +66,64 @@ class FlowNetC(nn.Module):
     def forward(self, x):
         x1 = x[:, :3]
         x2 = x[:, 3:]
-        with torch.no_grad():
-            out_conv2a = self.backboneModel(
-                    x1).permute(0, 3, 1, 2).contiguous()
-            out_conv2b = self.backboneModel(
-                    x2).permute(0, 3, 1, 2).contiguous()
 
-        out_conv3a = self.conv3(out_conv2a)
-        out_conv3b = self.conv3(out_conv2b)
+        outConvXa = [None]*3
+        outConvXb = [None]*3
+        outConvX = [None]*4
+        for i in range(len(self.model.stages)):
+            x1 = self.model.downsample_layers[i](x1)
+            x1 = self.model.stages[i](x1)
+            outConvXa[i] = x1
+        for i in range(len(self.model.stages)):
+            x2 = self.model.downsample_layers[i](x2)
+            x2 = self.model.stages[i](x2)
+            outConvXb[i] = x2
+        # out_conv1a = self.conv1(x1)
+        # out_conv2a = self.conv2(out_conv1a)
+        # out_conv3a = self.conv3(out_conv2a)
 
-        out_conv_redir = self.conv_redir(out_conv3a)
-        out_correlation = correlate(out_conv3a, out_conv3b)
+        # out_conv1b = self.conv1(x2)
+        # out_conv2b = self.conv2(out_conv1b)
+        # out_conv3b = self.conv3(out_conv2b)
+
+        # out_conv_redir = self.conv_redir(out_conv3a)
+        out_conv_redir = self.conv_redir(outConvXa[2])
+        out_correlation = correlate(outConvXa[2], outConvXb[2])
 
         in_conv3_1 = torch.cat([out_conv_redir, out_correlation], dim=1)
-
         out_conv3 = self.conv3_1(in_conv3_1)
-        out_conv4 = self.conv4(out_conv3)
-        out_conv5 = self.conv5(out_conv4)
-        out_conv6 = self.conv6(out_conv5)
+        x = out_conv3
+        for i in range(len(self.model2.stages)):
+            x = self.model2.downsample_layers[i](x)
+            x = self.model2.stages[i](x)
+            outConvX[i] = x
 
-        flow6 = self.predict_flow6(out_conv6)
-        flow6_up = crop_like(self.upsampled_flow6_to_5(flow6), out_conv5)
-        out_deconv5 = crop_like(self.deconv5(out_conv6), out_conv5)
+        #out_conv4 = self.conv4_1(self.conv4(out_conv3))
+        #out_conv5 = self.conv5_1(self.conv5(out_conv4))
+        #out_conv6 = self.conv6_1(self.conv6(out_conv5))
+        offset = 4
+        flow6 = self.predict_flow6(outConvX[6-offset])
+        flow6_up = crop_like(self.upsampled_flow6_to_5(flow6), outConvX[5-offset])
+        out_deconv5 = crop_like(self.deconv5(outConvX[6-offset]), outConvX[5-offset])
 
-        concat5 = torch.cat((out_conv5, out_deconv5, flow6_up), 1)
+        concat5 = torch.cat((outConvX[5-offset], out_deconv5, flow6_up), 1)
         flow5 = self.predict_flow5(concat5)
-        flow5_up = crop_like(self.upsampled_flow5_to_4(flow5), out_conv4)
-        out_deconv4 = crop_like(self.deconv4(concat5), out_conv4)
+        flow5_up = crop_like(
+            self.upsampled_flow5_to_4(flow5), outConvX[4-offset])
+        out_deconv4 = crop_like(self.deconv4(concat5), outConvX[4-offset])
 
-        concat4 = torch.cat((out_conv4, out_deconv4, flow5_up), 1)
+        concat4 = torch.cat((outConvX[4-offset], out_deconv4, flow5_up), 1)
         flow4 = self.predict_flow4(concat4)
-        flow4_up = crop_like(self.upsampled_flow4_to_3(flow4), out_conv3)
+        flow4_up = crop_like(
+            self.upsampled_flow4_to_3(flow4), out_conv3)
         out_deconv3 = crop_like(self.deconv3(concat4), out_conv3)
 
         concat3 = torch.cat((out_conv3, out_deconv3, flow4_up), 1)
         flow3 = self.predict_flow3(concat3)
-        flow3_up = crop_like(self.upsampled_flow3_to_2(flow3), out_conv2a)
-        out_deconv2 = crop_like(self.deconv2(concat3), out_conv2a)
+        flow3_up = crop_like(self.upsampled_flow3_to_2(flow3), outConvXa[1])
+        out_deconv2 = crop_like(self.deconv2(concat3), outConvXa[1])
 
-        concat2 = torch.cat((out_conv2a, out_deconv2, flow3_up), 1)
+        concat2 = torch.cat((outConvXa[1], out_deconv2, flow3_up), 1)
         flow2 = self.predict_flow2(concat2)
 
         if self.training:
